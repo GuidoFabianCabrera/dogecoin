25ec0c0019bd6c458ee97295e817580a43383d7f
Cabrera Guido <guidofabiancabrera@hotmail.com>
2021-09-04 20:19:46 -0300

    developing start

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../assets/styles/HomePage.scss';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default HomePage;
